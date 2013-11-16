fd213cb167601660083bc6962ebe62850ff199f3
miaowoo35 <hotdrinkconnie@hotmail.com>
2013-11-16 00:39:15 -0800

    Create 2SUM.java

---
+++ 2SUM.java
@@ -0,0 +1,19 @@
+    public int[] twoSumUnSorted(int[] numbers, int target) { 
+        HashMap<Integer, Integer> myMap=new HashMap<Integer, Integer>(); 
+        for (int i=0;i<numbers.length;i++){ 
+            myMap.put(numbers[i],i); 
+        } 
+         
+        int[] r=new int[2]; 
+        for(int i=0;i<numbers.length;i++){ 
+            int remain=target-numbers[i]; 
+            if (myMap.containsKey(remain) && i!=myMap.get(remain)){ //check when two same number add to target 
+                int tmp=i+myMap.get(remain)+2; //1 based 
+                r[0]=i<myMap.get(remain)?i+1:myMap.get(remain)+1; //store in order 
+                r[1]=tmp-r[0]; 
+            } 
+        } 
+        return r; 
+    }
+    
+    
