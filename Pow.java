b51ff87f0abdb267378320281fa37555abf1ce89
miaowoo35 <hotdrinkconnie@hotmail.com>
2013-11-16 00:18:31 -0800

    Update Pow.java

--- Pow.java
+++ Pow.java
@@ -1,27 +1,30 @@
-    public double pow(double x, int n) { 
-        if (n==0) return 1; 
-        if (n<0){  
-            x=1/x; 
-            n=-n; //need to /2 right away because Integer.min*-1 will overflow 
-        } 
-        double tmp = pow(x,n/2); 
-        return n%2==0?tmp*tmp:tmp*tmp*x; 
-    }
-    
-    public double pow(double x, int n) { 
-        if (n==0) return 1; 
-        if (n<0){  
-            x=1/x; 
-            n=-n; 
-        } 
-         
-        double product=1; 
-        while (n>0){ 
-            if (n%2==1) product*=x; //when items are odd, multiply each level's remainder 
-            x*=x; //each time power of 2 
-            n>>=1; 
-        } 
-        return product; 
-    } 
- 
-    
+	public double powRecursive(double x, int n) {
+		if (n == 0)
+			return 1;
+		if (n < 0) {
+			x = 1 / x;
+			n = -n; // need to /2 right away because Integer.min*-1 will
+					// overflow
+		}
+		double tmp = powRecursive(x, n / 2);
+		return n % 2 == 0 ? tmp * tmp : tmp * tmp * x;
+	}
+
+	public double powIterative(double x, int n) {
+		if (n == 0)
+			return 1;
+		if (n < 0) {
+			x = 1 / x;
+			n = -n;
+		}
+
+		double product = 1;
+		while (n > 0) {
+			if (n % 2 == 1)
+				product *= x; // when items are odd, multiply each level's
+								// remainder
+			x *= x; // each time power of 2
+			n >>= 1;
+		}
+		return product;
+	}
