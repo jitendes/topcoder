83d65887c42ee9c646f39927a11afd3cf4f3ffff
miaowoo35 <hotdrinkconnie@hotmail.com>
2013-11-16 16:00:13 -0800

    Create Sqrt.java

---
+++ Sqrt.java
@@ -0,0 +1,15 @@
+    public int sqrtLong(int x) {
+        long l=0;
+        long r=x;
+        while(l<=r){
+            long mid = (l+r)/2;
+            long s = mid*mid;
+            if(s==x)
+                return (int)mid;
+            if(s<x)
+                l = mid+1;
+            else
+                r = mid-1;
+        }
+        return l*l<=x?(int)l:(int)l-1; 
+    }
