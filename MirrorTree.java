9e47455d799eab66f76298730ca37fbd0b8b93ce
miaowoo35 <hotdrinkconnie@hotmail.com>
2013-11-19 00:55:11 -0800

    Update MirrorTree.java

--- MirrorTree.java
+++ MirrorTree.java
@@ -1,11 +1,9 @@
 public void mirrorTree(Node t) {
-		if (t == null)
-			return;
-		Node temp = null;
-		mirrorTree(t.left);
-		mirrorTree(t.right);
-		temp = t.left;
-		t.left = t.right;
-		t.right = temp;
-		return;
+	if (t == null) return;
+	mirrorTree(t.left);
+	mirrorTree(t.right);
+	Node temp = t.left;
+	t.left = t.right;
+	t.right = temp;
+	return;
 }
