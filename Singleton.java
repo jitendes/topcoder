914f72b74db78d35bf64f0303ee3de9e29c24bc9
miaowoo35 <hotdrinkconnie@hotmail.com>
2013-11-18 22:12:36 -0800

    Update Singleton.java

--- Singleton.java
+++ Singleton.java
@@ -12,3 +12,21 @@ public class Singleton {
 		return instance;
 	}
 }
+
+public class Singleton{
+	private static Singleton instance;
+	private Singleton(){
+		...
+	}
+
+	public static synchronized Singleton getInstance(){
+		if (instance == null)
+			instance = new Singleton();
+
+		return instance;
+	}
+	...
+	public void doSomething(){
+		...	
+	}
+}
