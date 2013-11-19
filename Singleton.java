2126e04da4ea540272727be11d4b8d1eddad5ed2
miaowoo35 <hotdrinkconnie@hotmail.com>
2013-11-18 22:37:42 -0800

    Update Singleton.java

--- Singleton.java
+++ Singleton.java
@@ -29,3 +29,31 @@ class SingletonThreadsafe {
 		// ...
 	}
 }
+
+//Lazy instantiation using double locking mechanism.
+class Singleton {
+	private static Singleton instance;
+
+	private Singleton() {
+		System.out.println("Singleton(): Initializing Instance");
+	}
+
+	public static Singleton getInstance() {
+		if (instance == null) {
+			synchronized (Singleton.class) {
+				if (instance == null) {
+					System.out
+							.println("getInstance(): First time getInstance was invoked!");
+					instance = new Singleton();
+				}
+			}
+		}
+
+		return instance;
+	}
+
+	public void doSomething() {
+		System.out.println("doSomething(): Singleton does something!");
+	}
+}
+
