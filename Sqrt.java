6dc6b7ed3112b2a400cb763ddb1dbfe0d6f7aa63
miaowoo35 <hotdrinkconnie@hotmail.com>
2013-11-19 01:01:36 -0800

    Update Sqrt.java

--- Sqrt.java
+++ Sqrt.java
@@ -1,3 +1,20 @@
+    public int sqrt(int x) {
+        if (x==0) return 0;
+        int l=0;
+        int r=x/4+1;
+        while(l<=r){
+            int mid = (l+r)/2;
+            if (mid==0) return 1; //divide by 0
+            if(mid==x/mid)
+                return mid;
+            if(mid<x/mid)
+                l = mid+1;
+            else
+                r = mid-1;
+        }
+        return (l+r)/2; 
+    }
+    
     public int sqrtLong(int x) {
         long l=0;
         long r=x/4+1;
@@ -14,7 +31,7 @@
         return (int)(l+r)/2; 
     }
     
-    public int sqrtInt(int x) {
+    public int sqrtInt1(int x) {
         int l=0;
         int r=x/4<(int)Math.sqrt(Integer.MAX_VALUE)? x/4+1:(int)Math.sqrt(Integer.MAX_VALUE);
         while(l<=r){
@@ -29,5 +46,6 @@
         }
         return (l+r)/2; 
     }
+
     
     
