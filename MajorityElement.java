4486a3015dd1ffaacb79154025a7de82df2b7857
belovedd <belovedd@users.noreply.github.com>
2013-11-19 10:53:45 -0800

    Update MajorityElement.java

--- MajorityElement.java
+++ MajorityElement.java
@@ -26,3 +26,16 @@ int findMajorityElement(int * arr, int size) {
      }
      else return -1;
 }
+
+// Given an array of 2n elements of which exactly n elements are same and the remaining n elements are all different. 
+// Find out the value which is present n times in the array.
+Function hasMajority(A[1:n]) 
+create a stack S; 
+push the first element of A onto S; 
+for i = 2:n 
+    if A[i] = the top element, push A[i] onto S; 
+    else pop the top element of S; 
+    if S is empty, push A[i] onto S; 
+    i++; 
+end for 
+return the top element of S;
