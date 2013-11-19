852ebe3dbcbcd81b65e8d11e030cff23f9eba124
miaowoo35 <hotdrinkconnie@hotmail.com>
2013-11-18 21:34:26 -0800

    Create BlockingQueue.java

---
+++ BlockingQueue.java
@@ -0,0 +1,41 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BlockingQueue<T> {
+	private Queue<T> queue = new LinkedList<T>();
+	private int capacity=10;
+
+	public BlockingQueue(int capacity) {
+		if (capacity<=0) throw new IllegalArgumentException(""+capacity);
+		this.queue=new LinkedList<T>();
+		this.capacity = capacity;
+	}
+
+	public synchronized void add(T item) throws InterruptedException {
+		while (isFull()) {
+			wait();
+		}
+		// Notify all the threads that are waiting
+		if (isEmpty()) {
+			notifyAll();
+		}
+		this.queue.add(item);
+	}
+
+	public synchronized Object remove() throws InterruptedException {
+		while (isEmpty()) {
+			wait();
+		}
+		if (isFull()) {
+			notifyAll();
+		}
+		return this.queue.remove(0);
+	} 
+  
+	private boolean isEmpty() {
+        return queue.size() == 0;
+    }
+    private boolean isFull() {
+        return queue.size() == this.capacity;
+    }
+}
